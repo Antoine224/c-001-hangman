c55035257dbd98f5f5468968226f220d84c922b2
Antoine224 <antoine.lebas@learner.42.tech>
2026-01-03 13:37:34 +0100

    Add new proto

--- lib/prototype.h
+++ lib/prototype.h
@@ -1,6 +1,12 @@
 #ifndef PROTOTYPE_H
 # define PROTOTYPE_H
 
+// cerveau, init la partie
+int		coreLogic(void);
 
+char	*requestFileName(void);
 
+// lib
+void ft_putstr(char *str);
+void ft_putchar(char c);
 #endif
