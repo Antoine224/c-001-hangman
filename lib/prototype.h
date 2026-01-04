f9766c694547ce1d4ca7cebb3b76b63928e6c6ae
Antoine224 <antoine.lebas@learner.42.tech>
2026-01-04 17:05:08 +0100

    Del useless fct

--- lib/prototype.h
+++ lib/prototype.h
@@ -3,17 +3,22 @@
 
 #include <stdio.h>
 #include "struct.h"
+
 // cerveau, init la partie
 int		core(game_t *game);
-
 
 char	*requestFileName(void);
 FILE	*findNameFile(void);
 char	*findWord(game_t *game);
 int		initGame(game_t *game);
 int		firstGame(void);
+
 // error
 int		error(char *str);
+
+// script
+//		french
+void	firstScrpit(void);
 
 // lib
 void	ft_putstr(char *str);
@@ -21,4 +26,5 @@ void	ft_putchar(char c);
 size_t	ft_strlen(char *str);
 char	*ft_strncpy(char *dst, const char *src, size_t size);
 char	*ft_strdup(char *str);
+
 #endif
