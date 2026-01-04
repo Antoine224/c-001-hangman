6ee22b455e4460f0c6311b72583a8862702f69d7
Antoine224 <antoine.lebas@learner.42.tech>
2026-01-04 17:08:25 +0100

    Add first page

--- src/game.c
+++ src/game.c
@@ -3,5 +3,6 @@
 int	core(game_t *game)
 {
 	//clear game
+	firstScrpit();
 	return (0);
 }
---
+++ src/print/french/script.c
@@ -0,0 +1,29 @@
+#include "../../../lib/header.h"
+
+void	firstScrpit(void)
+{
+	//clear shell here
+	ft_putstr("Contents\n========================================================================");
+	ft_putstr("\n[1]  New game");
+	ft_putstr("\n[2]  Setup");
+	ft_putstr("\n[3]  Exit\n\n");
+}
+
+/*
+Contents
+========================================================================
+[1]  New game
+[2]  Setup
+[3]  Exit
+
+========================================================================
+[1]  What's alt.ascii-art?  What's going on here?
+========================================================================
+    You're probably reading this because it's been posted to
+    news:alt.ascii-art, news:alt.ascii-art.animation or rec.arts.ascii.
+    If you're not, jump in and take a look.  In these Usenet groups
+    people discuss ASCII art, request ASCII art, post ASCII art, post
+    improved versions or variations of other people's ASCII art, and
+    generally have fun.
+
+*/
