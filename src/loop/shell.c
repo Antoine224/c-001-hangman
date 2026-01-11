a043c7509ccfac8d67c803286d9bf5390c4f44a3
Antoine224 <antoine.lebas@learner.42.tech>
2026-01-11 12:27:57 +0100

    Add choice for the menu

--- src/loop/shell.c
+++ src/loop/shell.c
@@ -1,16 +1,39 @@
 #include "../../lib/header.h"
 
+int	getNbr(void)
+{
+	char	buffer[80];
+	int		choice;
+	int		i;
+
+	i = 0;
+	if (read(0, &buffer, 79) == EOF)
+		return (-1);
+	while (ft_isdigit(buffer[i]))
+		i++;
+	buffer[i] = '\0';
+	if (i == 0 || i > 1)
+		return (-1);
+	return (ft_atoi(buffer));
+}
+
 int	loop(game_t *game)
 {
 	int	choice;
 
-	clear();
-	firstScrpit();
-	scanf("%d", &choice);
-	if (choice > 3 || choice < 1)
+	while (1)
 	{
-		invalidChoice(choice);
-		loop(game);
+		clear();
+		firstScrpit();
+		choice = getNbr();
+		if (choice > 0 && choice < 4)
+			break;
 	}
-	return (0);
+	if (choice == 1)
+		return (0);
+	if (choice == 2)
+		return (0);
+	if (choice == 3)
+		return (0);
+	return (1);
 }
